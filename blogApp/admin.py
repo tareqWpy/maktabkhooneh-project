fa06af091ee82332d8e5cd5ffc98e4a91f3b44bf
tareqWpy <126175759+tareqWpy@users.noreply.github.com>
2024-04-04 22:45:10 +0330

    Update 4.0
    added models and some views to admin pane;

--- blogApp/admin.py
+++ blogApp/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from blogApp.models import Post
+
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "published_date"
+    empty_value_display = "-empty-"
+    list_display = (
+        "title",
+        "counted_views",
+        "status",
+        "published_date",
+    )
+    list_filter = ("status",)
+    ordering = ["created_date"]
+    search_fields = ("title",)
+
+    # در صفحه دستکاری و ادیت فیلدهای پست به ورودی های زیر دسترسی داریم
+    # fields = ("title",)
+
+
+# admin.site.register(Post)
--- projectApp/admin.py
+++ projectApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from projectApp.models import Contact
+
+
+@admin.register(Contact)
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    pass
