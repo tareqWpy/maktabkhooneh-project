6b4ce388fa9ccfdbefcd8b856b2e189411b531c8
tareqWpy <126175759+tareqWpy@users.noreply.github.com>
2024-06-08 15:32:54 +0330

    2.4.0 version
    added pagination to the blog home page

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("", blog_home_view, name="blog-home"),
     path("category/<str:cat_name>/", blog_home_view, name="blog-category"),
     path("author/<str:author_username>/", blog_home_view, name="blog-author"),
+    path("search/", blog_search_view, name="blog-search"),
     path("blog-single/<int:pid>/", blog_single_view, name="blog-single"),
 ]
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,6 +1,5 @@
-from datetime import datetime
-from unicodedata import category
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
@@ -14,6 +13,31 @@ def blog_home_view(request, cat_name=None, author_username=None):
         posts = posts.filter(category__category_name=cat_name)
     if author_username is not None:
         posts = posts.filter(author__username=author_username)
+
+    posts = Paginator(posts, 4)
+    try:
+        page_number = request.GET.get("page")
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+    context = {"posts": posts}
+    return render(request, "blog/blog-home.html", context)
+
+
+def blog_search_view(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=1)
+    if request.method == "GET":
+        search = request.GET.get("s")
+        if search:
+            search = search.lower()
+            posts = posts.filter(
+                Q(content__contains=search)
+                | Q(author__username__contains=search)
+                | Q(title__contains=search)
+                | Q(category__category_name__contains=search)
+            ).distinct()
     context = {"posts": posts}
     return render(request, "blog/blog-home.html", context)
 
