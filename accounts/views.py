6c5ffc1244bdfa85c27befabb0e1a41d70f406f9
tareqWpy <126175759+tareqWpy@users.noreply.github.com>
2024-06-15 15:18:38 +0330

    Bug fixes
    added messages to login and signup form

--- accounts/views.py
+++ accounts/views.py
@@ -18,14 +18,22 @@ def login_view(request):
                 if user is not None:
                     login(request, user)
                     if "next" in request.POST:
+                        messages.success(request, "You logged in successfully! 😀")
                         return redirect(request.POST.get("next"))
                     else:
+                        messages.success(request, "You logged in successfully! 😀")
                         return redirect("/")
-        messages.success(
-            request,
-            "You logged in successfully!",
-        )
-        form = AuthenticationForm()
+                else:
+                    messages.error(
+                        request, "Invalid username or password. Please try again! ❌"
+                    )
+            else:
+                messages.error(
+                    request,
+                    "Invalid form submission. Please check the entered data! ❌",
+                )
+        else:
+            form = AuthenticationForm()
         context = {"form": form}
         return render(request, "accounts/login.html", context)
     else:
@@ -42,17 +50,22 @@ def logout_view(request):
     return redirect("/")
 
 
+from django.contrib import messages
+
+
 def signup_view(request):
     if not request.user.is_authenticated:
         if request.method == "POST":
             form = RegisterForm(request.POST)
             if form.is_valid():
                 form.save()
+                messages.success(request, "You signed up successfully! ✅")
                 return redirect("accounts:login")
-        messages.success(
-            request,
-            "You signed up successfully!",
-        )
+            else:
+                messages.error(
+                    request, "Errors in the form. Please correct them and try again!❌"
+                )
+
         form = RegisterForm()
         context = {"form": form}
         return render(request, "accounts/signup.html", context)
