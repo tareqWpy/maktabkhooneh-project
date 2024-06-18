408c07485a4c1627b8600bf3c17c3042a9ecee84
tareqWpy <126175759+tareqWpy@users.noreply.github.com>
2024-06-18 23:55:22 +0330

    bug fixes

--- accounts/urls.py
+++ accounts/urls.py
@@ -26,22 +26,8 @@ urlpatterns = [
         name="password_reset",
     ),
     path(
-        "password_reset_done/",
-        auth_views.PasswordResetDoneView.as_view(
-            template_name="accounts/password_reset_done.html"
-        ),
-        name="password_reset_done",
-    ),
-    path(
         "password_reset_confirm/<uidb64>/<token>/",
         CustomPasswordResetConfirmView.as_view(),
         name="password_reset_confirm",
-    ),
-    path(
-        "password_reset_complete/",
-        auth_views.PasswordResetCompleteView.as_view(
-            template_name="accounts/password_reset_complete.html"
-        ),
-        name="password_reset_complete",
     ),
 ]
